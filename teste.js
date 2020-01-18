2bcbfdba8aee28f80899e2e7f788dccc3a33d495
TiagoMoreira-top <tdfmoreira@gmail.com>
2020-01-18 17:21:56 +0000

    added a home html and js

--- teste.js
+++ teste.js
@@ -1,13 +1,22 @@
+const getExerciseByCategory = async (category) => {
+    const data = await fetch(`http://my-json-server.typicode.com/TiagoMoreira-top/Projeto-Tecnologias-Web/running`)
+    const json = await data.json()
+}
+getExerciseByCategory()
+
 async function abc() {
     const data = await fetch(`http://my-json-server.typicode.com/TiagoMoreira-top/Projeto-Tecnologias-Web/categories`)
     const json = await data.json()
     console.log(json);
+    for (let i = 0; i < json.length; i++) {
+        document.getElementById("sltCategory").innerHTML += `<option> ${json[i].strCategory} </option>`
+        document.getElementById("btnExercises").addEventListener("click", function() {
+            const categoryElement = document.querySelector("#sltCategory")
+            const categoryValue = categoryElement.options[categoryElement.selectedIndex].text
+            getExerciseByCategory(categoryValue)
+        })
+    }
 }
 abc()
 
-async function abcd() {
-    const data = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`)
-    const json = await data.json()
-    console.log(json);
-}
-abcd()
+
