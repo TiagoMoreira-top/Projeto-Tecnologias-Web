fed7f778cfabc92365fbe03ba3716738f97311fa
TiagoMoreira-top <tdfmoreira@gmail.com>
2020-01-18 17:35:33 +0000

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,8 +1,8 @@
 const getExerciseByCategory = async (category) => {
-    const data = await fetch(`http://my-json-server.typicode.com/TiagoMoreira-top/Projeto-Tecnologias-Web/running`)
+    const data = await fetch(`http://my-json-server.typicode.com/TiagoMoreira-top/Projeto-Tecnologias-Web/${category}`)
     const json = await data.json()
+    console.log(json);
 }
-getExerciseByCategory()
 
 async function abc() {
     const data = await fetch(`http://my-json-server.typicode.com/TiagoMoreira-top/Projeto-Tecnologias-Web/categories`)
@@ -10,13 +10,14 @@ async function abc() {
     console.log(json);
     for (let i = 0; i < json.length; i++) {
         document.getElementById("sltCategory").innerHTML += `<option> ${json[i].strCategory} </option>`
-        document.getElementById("btnExercises").addEventListener("click", function() {
-            const categoryElement = document.querySelector("#sltCategory")
-            const categoryValue = categoryElement.options[categoryElement.selectedIndex].text
-            getExerciseByCategory(categoryValue)
-        })
+
     }
 }
 abc()
 
+document.getElementById("btnExercises").addEventListener("click", function() {
+    const categoryElement = document.querySelector("#sltCategory")
+    const categoryValue = categoryElement.options[categoryElement.selectedIndex].text
+    getExerciseByCategory(categoryValue)
+})
 
