e27f0de18ff72705915717061e35c3c6eb90fa17
TiagoMoreira-top <tdfmoreira@gmail.com>
2020-01-18 17:48:59 +0000

    Update teste.js

--- teste.js
+++ teste.js
@@ -4,7 +4,7 @@ const getExerciseByCategory = async (category) => {
     console.log(json);
 }
 
-async function abc() {
+async function getCategories() {
     const data = await fetch(`http://my-json-server.typicode.com/TiagoMoreira-top/Projeto-Tecnologias-Web/categories`)
     const json = await data.json()
     console.log(json);
@@ -13,7 +13,7 @@ async function abc() {
 
     }
 }
-abc()
+getCategories()
 
 document.getElementById("btnExercises").addEventListener("click", function() {
     const categoryElement = document.querySelector("#sltCategory")
