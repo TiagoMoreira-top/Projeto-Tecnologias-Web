c80368f1fef4f3647d426199faea3198f7779c8c
TiagoMoreira-top <tdfmoreira@gmail.com>
2020-01-19 13:26:39 +0000

    visual changes

--- teste.js
+++ teste.js
@@ -1,9 +1,7 @@
 const getExerciseByCategory = async (category) => {
     const data = await fetch(`http://my-json-server.typicode.com/TiagoMoreira-top/Projeto-Tecnologias-Web/${category}`)
     const json = await data.json()
-    console.log(json);
     const exercises = json[0].exercises
-    console.log(exercises);
     document.getElementById("exercises").innerHTML = ""
     let color = "grey"
     for (let i = 0; i < exercises.length; i++) {
@@ -14,17 +12,17 @@ const getExerciseByCategory = async (category) => {
         const repetitions = exercises[i][`repetitions`]
         const series = exercises[i][`series`]
         if (exercise && color == "grey") {
-            document.getElementById("exercises").innerHTML += `<div class="text-light bg-secondary rounded-left rounded-right"> <div id="${id}"> <p> <span> EXERCÍCIO </span>: <span class="font-weight-bold"> ${exercise} </span> </div> <p> Descrição: ${execution} </p> <p> Nº de repetições: ${repetitions} <p> Nº de Séries: ${series} </p> </p> </p> <a class="bg-danger text-light rounded" href="${youtube}"> YouTube Tutorial </a> </div>`
-        document.getElementById(`${id}`).classList.add("text-uppercase")
-        color = "dark"
+            document.getElementById("exercises").innerHTML += `<div class="text-light bg-secondary rounded-left rounded-right"> <div id="${id}"> <p> <span> EXERCÍCIO </span>: <span class="font-weight-bold"> ${exercise} </span> </div> <p> Descrição: ${execution} </p> <p> Nº de repetições: ${repetitions} <p> Nº de Séries: ${series} </p> </p> </p> <a class="bg-danger text-light rounded" href="${youtube}"> YouTube Tutorial </a> </div> <br>`
+            document.getElementById(`${id}`).classList.add("text-uppercase")
+            color = "dark"
         } else if (exercise && color == "dark") {
-            document.getElementById("exercises").innerHTML += `<div class="text-light lightGrey rounded-left rounded-right"> <div id="${id}"> <p> <span> EXERCÍCIO </span>: <span class="font-weight-bold"> ${exercise} </span> </div> <p> Descrição: ${execution} </p> <p> Nº de repetições: ${repetitions} <p> Nº de Séries: ${series} </p> </p> </p> <a class="bg-danger text-light rounded" href="${youtube}"> YouTube Tutorial </a> </div>`
-        document.getElementById(`${id}`).classList.add("text-uppercase")
-        color = "grey"
-        } 
+            document.getElementById("exercises").innerHTML += `<div class="text-light lightGrey rounded-left rounded-right"> <div id="${id}"> <p> <span> EXERCÍCIO </span>: <span class="font-weight-bold"> ${exercise} </span> </div> <p> Descrição: ${execution} </p> <p> Nº de repetições: ${repetitions} <p> Nº de Séries: ${series} </p> </p> </p> <a class="bg-danger text-light rounded" href="${youtube}"> YouTube Tutorial </a> </div> <br>`
+            document.getElementById(`${id}`).classList.add("text-uppercase")
+            color = "grey"
+        }
     }
-    document.getElementById("exercises").innerHTML += ` <br> <br> <button class="btn btn-secondary">Plano Alimentar</button>`
-    document.getElementById("exercises").addEventListener("click", function() {
+    document.getElementById("exercises").innerHTML += ` <br> <button class="btn btn-secondary">Plano Alimentar</button> <br> <br>`
+    document.getElementById("exercises").addEventListener("click", function () {
 
     })
 }
@@ -39,7 +37,7 @@ async function getCategories() {
 }
 getCategories()
 
-document.getElementById("btnExercises").addEventListener("click", function() {
+document.getElementById("btnTrainningPlan").addEventListener("click", function () {
     const categoryElement = document.querySelector("#sltCategory")
     const categoryValue = categoryElement.options[categoryElement.selectedIndex].text
     getExerciseByCategory(categoryValue)
@@ -50,3 +48,15 @@ async function getEatingPlan(category) {
     const json = data.json()
     console.log(json);
 }
+
+document.getElementById("btnUserWeight").addEventListener("click", function () {
+    const categoryElement = document.querySelector("#sltWeight")
+    const categoryValue = categoryElement.options[categoryElement.selectedIndex].text
+    document.getElementById("btnUserWeight").classList.add("d-none")
+    document.getElementById("btnUndoUserWeight").classList.remove("d-none")
+})
+
+document.getElementById("btnUndoUserWeight").addEventListener("click", function () {
+    document.getElementById("btnUserWeight").classList.remove("d-none")
+    document.getElementById("btnUndoUserWeight").classList.add("d-none")
+})
