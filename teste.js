c8ffd480fb30e746764c714d019602ef5dc39783
TiagoMoreira-top <tdfmoreira@gmail.com>
2020-01-19 13:45:25 +0000

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,7 +1,50 @@
 const getExerciseByCategory = async (category) => {
     const data = await fetch(`http://my-json-server.typicode.com/TiagoMoreira-top/Projeto-Tecnologias-Web/${category}`)
     const json = await data.json()
-    const exercises = json[0].exercises
+    document.getElementById("exercises").addEventListener("click", function () {
+    })
+}
+
+async function getCategories() {
+    const data = await fetch(`http://my-json-server.typicode.com/TiagoMoreira-top/Projeto-Tecnologias-Web/categories`)
+    const json = await data.json()
+    for (let i = 0; i < json.length; i++) {
+        document.getElementById("sltCategory").innerHTML += `<option value="${i}"> ${json[i].strCategory} </option>`
+
+    }
+}
+getCategories()
+
+document.getElementById("btnTrainningPlan").addEventListener("click", function () {
+    const categoryElement = document.querySelector("#sltCategory")
+    const categoryValue = categoryElement.options[categoryElement.selectedIndex].value
+  //  getExerciseByCategory(categoryValue)
+  getExercicesCategories(categoryValue)
+})
+
+async function getEatingPlan(category) {
+    const data = await fecth(`http://my-json-server.typicode.com/TiagoMoreira-top/Projeto-Tecnologias-Web/${category}`)
+    const json = data.json()
+    console.log(json);
+}
+
+document.getElementById("btnUserWeight").addEventListener("click", function () {
+    const categoryElement = document.querySelector("#sltWeight")
+    const categoryValue = categoryElement.options[categoryElement.selectedIndex].text
+    document.getElementById("btnUserWeight").classList.add("d-none")
+    document.getElementById("btnUndoUserWeight").classList.remove("d-none")
+})
+
+document.getElementById("btnUndoUserWeight").addEventListener("click", function () {
+    document.getElementById("btnUserWeight").classList.remove("d-none")
+    document.getElementById("btnUndoUserWeight").classList.add("d-none")
+})
+
+async function getExercicesCategories(value) {
+    const data = await fetch(`http://my-json-server.typicode.com/TiagoMoreira-top/Projeto-Tecnologias-Web/categories`)
+    const json = await data.json()
+    console.log(json[value].exercises);
+    const exercises = json[value].exercises
     document.getElementById("exercises").innerHTML = ""
     let color = "grey"
     for (let i = 0; i < exercises.length; i++) {
@@ -22,41 +65,4 @@ const getExerciseByCategory = async (category) => {
         }
     }
     document.getElementById("exercises").innerHTML += ` <br> <button class="btn btn-secondary">Plano Alimentar</button> <br> <br>`
-    document.getElementById("exercises").addEventListener("click", function () {
-
-    })
 }
-
-async function getCategories() {
-    const data = await fetch(`http://my-json-server.typicode.com/TiagoMoreira-top/Projeto-Tecnologias-Web/categories`)
-    const json = await data.json()
-    for (let i = 0; i < json.length; i++) {
-        document.getElementById("sltCategory").innerHTML += `<option> ${json[i].strCategory} </option>`
-
-    }
-}
-getCategories()
-
-document.getElementById("btnTrainningPlan").addEventListener("click", function () {
-    const categoryElement = document.querySelector("#sltCategory")
-    const categoryValue = categoryElement.options[categoryElement.selectedIndex].text
-    getExerciseByCategory(categoryValue)
-})
-
-async function getEatingPlan(category) {
-    const data = await fecth(`http://my-json-server.typicode.com/TiagoMoreira-top/Projeto-Tecnologias-Web/${category}`)
-    const json = data.json()
-    console.log(json);
-}
-
-document.getElementById("btnUserWeight").addEventListener("click", function () {
-    const categoryElement = document.querySelector("#sltWeight")
-    const categoryValue = categoryElement.options[categoryElement.selectedIndex].text
-    document.getElementById("btnUserWeight").classList.add("d-none")
-    document.getElementById("btnUndoUserWeight").classList.remove("d-none")
-})
-
-document.getElementById("btnUndoUserWeight").addEventListener("click", function () {
-    document.getElementById("btnUserWeight").classList.remove("d-none")
-    document.getElementById("btnUndoUserWeight").classList.add("d-none")
-})
