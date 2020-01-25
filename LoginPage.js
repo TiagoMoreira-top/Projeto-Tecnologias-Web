f2bafb5c7a511a52f41863a7e470203bb4291f26
TiagoMoreira-top <tdfmoreira@gmail.com>
2020-01-25 21:53:43 +0000

    Update LoginPage.js

--- LoginPage.js
+++ LoginPage.js
@@ -4,26 +4,33 @@ function confirmUser() {
 
     const userEmail = document.getElementById("userEmail").value
     const userPassword = document.getElementById("userPassword").value
-
-    for (let i = 0; i < JSON.parse(localStorage.getItem("usersData")).length; i++) {
-        console.log(JSON.parse(localStorage.getItem("usersData"))[i].email);
-        console.log(userEmail);
-        if (userEmail == JSON.parse(localStorage.getItem("usersData"))[i].email) {
-            if (userPassword == JSON.parse(localStorage.getItem("usersData"))[i].password) {
-                document.getElementById("alertBoxDanger").classList.add("d-none")
-                document.getElementById("alertBoxDanger").classList.remove("d-flex")
-                key = i
-                localStorage.setItem("user", i)
-                window.location.href = "teste.html";
+    if (JSON.parse(localStorage.getItem("usersData"))) {
+        for (let i = 0; i < JSON.parse(localStorage.getItem("usersData")).length; i++) {
+            console.log(JSON.parse(localStorage.getItem("usersData"))[i].email);
+            console.log(userEmail);
+            if (userEmail == JSON.parse(localStorage.getItem("usersData"))[i].email) {
+                if (userPassword == JSON.parse(localStorage.getItem("usersData"))[i].password) {
+                    document.getElementById("alertBoxDanger").classList.add("d-none")
+                    document.getElementById("alertBoxDanger").classList.remove("d-flex")
+                    key = i
+                    localStorage.setItem("user", i)
+                    window.location.href = "teste.html";
+                } else {
+                    document.getElementById("alertBoxDanger").classList.remove("d-none")
+                    document.getElementById("alertBoxDanger").classList.add("d-flex")
+                }
             } else {
                 document.getElementById("alertBoxDanger").classList.remove("d-none")
                 document.getElementById("alertBoxDanger").classList.add("d-flex")
             }
         }
+    } else {
+        document.getElementById("alertBoxDanger").classList.remove("d-none")
+        document.getElementById("alertBoxDanger").classList.add("d-flex")
     }
 }
 
-document.getElementById("btnSubmit").addEventListener("click", function(e) {
+document.getElementById("btnSubmit").addEventListener("click", function (e) {
     if (document.getElementById("btnSubmit").checkValidity() == true) {
         e.preventDefault()
         confirmUser()
