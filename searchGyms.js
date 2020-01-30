6e4c35d9b04523cda22a1cd6003b270eb921e3f8
TiagoMoreira-top <tdfmoreira@gmail.com>
2020-01-30 18:23:36 +0000

    search gyms functioning

--- searchGyms.js
+++ searchGyms.js
@@ -1,11 +1,11 @@
-apiKey = "AIzaSyBagazMTGHYCgDIHRAgoYEM_w8UPmAtEvA"
+apiKey = "AIzaSyAA1nMi1QDSZ1w7pJ6InpthhFgM9zVeaCI"
 let userLat
 let userLon
 let service
 
 getLocation()
 
-function initialize() {
+/* function initialize() {
 
 const userLocation = new google.maps.LatLng(userLat, userLon);
 
@@ -18,13 +18,15 @@ const request = {
   service = new google.maps.places.PlacesService(document.getElementById("abc"));
   service.nearbySearch(request, callback)
   
-}
+} */
 
 function callback(results, status) {
    console.log("entrou");
     if (status == google.maps.places.PlacesServiceStatus.OK) {
         console.log(results);
-        console.log("entrou");
+        for (let i = 0; i < results.length; i++) {
+         document.getElementById("gyms").innerHTML += ` <div id="${i}"> <h1> ${results[i].name} <p> ${results[i].rating} </p> <div>  </div> </h1> </div>`
+        }
     }
 }
 
@@ -41,55 +43,102 @@ function showPosition(position) {
 
 let myResults = [
    {
-      "geometry": {
-         "location": {
-            "lat": -33.8667822,
-            "lng": 151.1928329
+      "geometry" : {
+         "location" : {
+            "lat" : 41.1333818,
+            "lng" : -8.560787099999999
          },
-         "viewport": {
-            "northeast": {
-               "lat": -33.8654122197085,
-               "lng": 151.1941653802915
+         "viewport" : {
+            "northeast" : {
+               "lat" : 41.1346980302915,
+               "lng" : -8.559441669708496
             },
-            "southwest": {
-               "lat": -33.8681101802915,
-               "lng": 151.1914674197085
+            "southwest" : {
+               "lat" : 41.1320000697085,
+               "lng" : -8.562139630291501
             }
          }
       },
-      "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
-      "id": "51488d14e05c68e4a2d669549391ec4f96f0bb23",
-      "name": "Personal Trainer Pyrmont",
-      "opening_hours": {
-         "open_now": false
-      },
-      "photos": [
+      "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/school-71.png",
+      "id" : "b8858143e2e2235c226eb3e19eb54c31116172f3",
+      "name" : "Pavilhão Escola Secundária de Valbom",
+      "photos" : [
          {
-            "height": 626,
-            "html_attributions": [
-               "\u003ca href=\"https://maps.google.com/maps/contrib/108137823778339419046\"\u003ePersonal Trainer Pyrmont\u003c/a\u003e"
+            "height" : 1080,
+            "html_attributions" : [
+               "\u003ca href=\"https://maps.google.com/maps/contrib/103755504398272767805\"\u003eVitor Fernandes\u003c/a\u003e"
             ],
-            "photo_reference": "CmRaAAAAiUKfThEtMX78qx-s_k9hxg-tUdaR0i59qV80fgxY5oxIjzMrY_KPO73mx8pRI9k5BXhllXaoLSvw76cJjj7Ug3chJHWeqNHWGyuJTL4F5nmtlM5kFFY6gn96imqhs_e1EhC4Ueu3BhMWz4xln331ahn7GhR7tov-UYMg1HR-vNClmfYlQSDtug",
-            "raw_reference": {
-               "fife_url": "https://lh3.googleusercontent.com/p/AF1QipNGLXOXFSigkDZO2nkt6gvKoXfx0GhEYei4N5hb=k"
+            "photo_reference" : "CmRaAAAA5L5N9PzZWK5TmaMmVITzLSngEk_K6UZa_9eOZ-TJ4F_j-Qemlr4IEE5zNJnFK2nkeCECxwbxYRa8kpYNgSum6J2TTY0sTvH3gxHvA_7VbgvsVfAC2Z_cb3889Y0xcRP3EhCyPAfiJWz3ZV0lMmDjCOEsGhQZNU8GWrP6kUO8wJe_WcfLjjuntQ",
+            "raw_reference" : {
+               "fife_url" : "https://lh3.googleusercontent.com/p/AF1QipNzNeUaq2SZol-Adieg1gqBrbIjR0IJXnPbpSJI=k"
             },
-            "width": 872
+            "width" : 1920
          }
       ],
-      "place_id": "ChIJmUa5eDauEmsRb1CFYXexFvY",
-      "plus_code": {
-         "compound_code": "45MV+74 Pyrmont, New South Wales, Austrália",
-         "global_code": "4RRH45MV+74"
+      "place_id" : "ChIJD7jeU0VjJA0RnMw3NWZ-YyI",
+      "plus_code" : {
+         "compound_code" : "4CMQ+9M Valbom, Portugal",
+         "global_code" : "8CHH4CMQ+9M"
       },
-      "rating": 5,
-      "reference": "ChIJmUa5eDauEmsRb1CFYXexFvY",
-      "scope": "GOOGLE",
-      "types": ["gym", "health", "point_of_interest", "establishment"],
-      "user_ratings_total": 1,
-      "vicinity": "Scott Street, Pyrmont"
+      "rating" : 4.5,
+      "reference" : "ChIJD7jeU0VjJA0RnMw3NWZ-YyI",
+      "scope" : "GOOGLE",
+      "types" : [ "gym", "health", "point_of_interest", "establishment" ],
+      "user_ratings_total" : 2,
+      "vicinity" : "Rua Teixeira Lopes, Valbom"
+   },
+   {
+      "geometry" : {
+         "location" : {
+            "lat" : 41.12967310000001,
+            "lng" : -8.5613911
+         },
+         "viewport" : {
+            "northeast" : {
+               "lat" : 41.13098893029151,
+               "lng" : -8.560075769708499
+            },
+            "southwest" : {
+               "lat" : 41.12829096970851,
+               "lng" : -8.562773730291504
+            }
+         }
+      },
+      "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+      "id" : "87f728ae757de5f232cc737d6aedf8fbe351b076",
+      "name" : "Corpo & Exercício",
+      "photos" : [
+         {
+            "height" : 3024,
+            "html_attributions" : [
+               "\u003ca href=\"https://maps.google.com/maps/contrib/103046419763098127248\"\u003eChristopher Falk\u003c/a\u003e"
+            ],
+            "photo_reference" : "CmRaAAAA7mpFPFdNhj57b61JB-jJjBPWc63ruXU2UVeCdUkXxddZIVjSl5eE1QsUKC8NLAmhJLDw4frHvorHl4E42di3tmvabf5_OsmRIcocsxRrQCn_06JPmZhHahR34jg1dgDBEhDajOKjO9-_f61x62YOgmjnGhRUNSE0s_g1sb6W88uv2rXtb4Bmgg",
+            "raw_reference" : {
+               "fife_url" : "https://lh3.googleusercontent.com/p/AF1QipNK_hgQhYhv1DojgKWLx7Y3fHelsqlKegZHEN8d=k"
+            },
+            "width" : 4032
+         }
+      ],
+      "place_id" : "ChIJ-VyE2VFjJA0RGASKRhnXnug",
+      "plus_code" : {
+         "compound_code" : "4CHQ+VC Valbom, Portugal",
+         "global_code" : "8CHH4CHQ+VC"
+      },
+      "rating" : 4.5,
+      "reference" : "ChIJ-VyE2VFjJA0RGASKRhnXnug",
+      "scope" : "GOOGLE",
+      "types" : [ "gym", "health", "point_of_interest", "establishment" ],
+      "user_ratings_total" : 16,
+      "vicinity" : "Rua Clube Naval Infante Dom Henrique 438, Valbom"
    }
 ]
-
+function showGyms() {
+   document.getElementById("gyms").innerHTML = ""
+for (let i = 0; i < myResults.length; i++) {
+   document.getElementById("gyms").innerHTML += ` <div id="${i}" class="text-light border rounded blue"> <br> <h5> ${myResults[i].name} </h5> <br> <p> Rating: <span class="font-weight-bold"> ${myResults[i].rating} </span> </p> <img class="images" src="${myResults[i].photos[0].raw_reference.fife_url}"> <div> <br> <br>`
+  }
+}
 console.log(myResults);
 console.log(myResults[0].name);
 console.log(myResults[0].rating);
