b9f6ea9f193c4e5da4cdae6c2c62a3c8bd4bd461
TiagoMoreira-top <tdfmoreira@gmail.com>
2020-01-30 23:16:11 +0000

    localStorage Terminar sessão

--- teste.js
+++ teste.js
@@ -31,13 +31,19 @@ async function getEatingPlan(value) {
     console.log(json);
     document.getElementById("exercises").innerHTML = ""
     document.getElementById("eatingPlan").innerHTML = ""
+    document.getElementById("categoryTitle").innerHTML = ""
+    document.getElementById("categoryTitle").innerHTML = `<br> <h1 id="dinamicCategoryTitle" class="font-weight-bold row justify-content-center"> ${json[value].strCategory} </h1>`
+    document.getElementById(`dinamicCategoryTitle`).addEventListener("click", function() {
+        document.getElementById("eatingPlan").innerHTML = ""
+        document.getElementById("categoryTitle").innerHTML = ""
+    })
     console.log(json[value].breakfast[0].meal);
     const breakfast = json[value].breakfast
     const lunch = json[value].lunch
     const dinner = json[value].dinner
-    document.getElementById("eatingPlan").innerHTML += `Pequeno Almoço: <br> <div class="d-flex justify-content-center flex-wrap text-center" id="breakfastOptions"> </div>`
-    document.getElementById("eatingPlan").innerHTML += `Almoço: <br> <div id="lunchOptions" class="d-flex justify-content-center flex-wrap text-center"> </div>`
-    document.getElementById("eatingPlan").innerHTML += `Jantar: <br> <div id="dinnerOptions" class="d-flex justify-content-center flex-wrap text-center"> </div>`
+    document.getElementById("eatingPlan").innerHTML += ` <br> <h5 class="font-weight-bold"> Pequeno Almoço: </h5> <br> <div class="d-flex justify-content-center flex-wrap text-center" id="breakfastOptions"> </div>`
+    document.getElementById("eatingPlan").innerHTML += ` <br> <h5 class="font-weight-bold"> Almoço: </h5> <br> <div id="lunchOptions" class="d-flex justify-content-center flex-wrap text-center"> </div>`
+    document.getElementById("eatingPlan").innerHTML += ` <br> <h5 class="font-weight-bold"> Jantar: </h5> <br> <div id="dinnerOptions" class="d-flex justify-content-center flex-wrap text-center"> </div>`
     for (let i = 0; i < breakfast.length; i++) {
         document.getElementById("breakfastOptions").innerHTML += `<div class="text-light blue rounded-left rounded-right border"> <p> ${breakfast[i].meal} </p></div>`
         document.getElementById("lunchOptions").innerHTML += ` <div class="text-light blue rounded-left rounded-right border"><p> ${lunch[i].meal} </p></div> `
@@ -53,7 +59,11 @@ async function getExercicesByCategory(value) {
     document.getElementById("eatingPlan").innerHTML = ""
     document.getElementById("exercises").innerHTML = ""
     document.getElementById("categoryTitle").innerHTML = ""
-    document.getElementById("categoryTitle").innerHTML = `<br> <h1 class="font-weight-bold row justify-content-center"> ${json[value].strCategory} </h1> <br>`
+    document.getElementById("categoryTitle").innerHTML = `<br> <h1 id="dinamicCategoryTitle" class="font-weight-bold row justify-content-center"> ${json[value].strCategory} </h1> <br>`
+    document.getElementById(`dinamicCategoryTitle`).addEventListener("click", function() {
+        document.getElementById("exercises").innerHTML = ""
+        document.getElementById("categoryTitle").innerHTML = ""
+    })
     let color = "grey"
     for (let i = 0; i < exercises.length; i++) {
         const exercise = exercises[i][`strExercise`]
@@ -72,7 +82,6 @@ async function getExercicesByCategory(value) {
             color = "grey"
         }
     }
-    
 
     /*  const images = json[value].images
      console.log(images);
@@ -125,3 +134,7 @@ function changeTheme() {
 const user = localStorage.getItem("user")
 const name = JSON.parse(localStorage.getItem("usersData"))[user].name
 document.getElementById("user").innerHTML = name
+
+function deleteUserData() {
+    localStorage.removeItem("user")
+}
